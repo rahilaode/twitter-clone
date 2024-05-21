5edc0314ecbbad9275122b8312be54fce64df1f1
rahilaode <laoderahilatansa@gmail.com>
2024-05-21 16:18:44 +0700

    testing: change find element by XPATH

--- tests/login.py
+++ tests/login.py
@@ -5,10 +5,10 @@ def valid_login():
     try:
         driver = chrome_driver()
         driver.get("http://localhost:5000/login")
-        driver.find_element(By.XPATH, "//input[@name=\'username\']").click()
-        driver.find_element(By.XPATH, "//input[@name=\'username\']").send_keys("xyzabc")
-        driver.find_element(By.XPATH, "//input[@name=\'password\']").click()
-        driver.find_element(By.XPATH, "//input[@name=\'password\']").send_keys("123456")
+        driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label/input").click()
+        driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label/input").send_keys("xyz")
+        driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label[2]/input").click()
+        driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label[2]/input").send_keys("123456")
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/button").click()
         # driver.implicitly_wait(30)
         # driver.find_element(By.XPATH, "//span[contains(.,\'Home\')]").click()
@@ -25,10 +25,10 @@ def invalid_login():
     try:
         driver = chrome_driver()
         driver.get("http://localhost:5000/login")
-        driver.find_element(By.XPATH, "//input[@name=\'username\']").click()
-        driver.find_element(By.XPATH, "//input[@name=\'username\']").send_keys("abcdefg") # Try login with non existing user
-        driver.find_element(By.XPATH, "//input[@name=\'password\']").click()
-        driver.find_element(By.XPATH, "//input[@name=\'password\']").send_keys("123456")
+        driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label/input").click()
+        driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label/input").send_keys("abc") #Try login with non existing user
+        driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label[2]/input").click()
+        driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label[2]/input").send_keys("123456")
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/button").click()
         driver.implicitly_wait(30)
         # driver.find_element(By.XPATH, "//span[contains(.,\'Home\')]").click()
