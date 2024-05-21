b64db37d7571b529ceae23217b10410335d02205
rahilaode <laoderahilatansa@gmail.com>
2024-05-21 16:32:31 +0700

    testing: change implicity_wait to 60

--- tests/signup.py
+++ tests/signup.py
@@ -5,7 +5,7 @@ def valid_signup():
     try:
         driver = chrome_driver()
         driver.get("http://localhost:5000/signup")
-        driver.implicitly_wait(30)
+        driver.implicitly_wait(60)
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label/input").click()
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label/input").send_keys("xyz@gmail.com")
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/div/label/input").click()
@@ -15,7 +15,7 @@ def valid_signup():
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label[2]").click()
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label[2]/input").send_keys("123456")
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/button").click()
-        # driver.implicitly_wait(30)
+        # driver.implicitly_wait(60)
         # driver.find_element(By.XPATH, "//span[contains(.,\'Home\')]").click()
         # driver.find_element(By.XPATH, "//span[contains(.,\'Notifications\')]").click()
         # driver.find_element(By.XPATH, "//span[contains(.,\'Profile\')]").click()
@@ -30,7 +30,7 @@ def invalid_signup():
     try:
         driver = chrome_driver()
         driver.get("http://localhost:5000/signup")
-        driver.implicitly_wait(30)
+        driver.implicitly_wait(60)
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label/input").click()
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label/input").send_keys("xyzagmail.com") # Try don't use @ sign
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/div/label/input").click()
@@ -40,7 +40,7 @@ def invalid_signup():
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label[2]").click()
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/label[2]/input").send_keys("123456")
         driver.find_element(By.XPATH, "//div[@id=\'root\']/div/div/div[2]/form/button").click()
-        # driver.implicitly_wait(30)
+        # driver.implicitly_wait(60)
         # driver.find_element(By.XPATH, "//span[contains(.,\'Home\')]").click()
         # driver.find_element(By.XPATH, "//span[contains(.,\'Notifications\')]").click()
         # driver.find_element(By.XPATH, "//span[contains(.,\'Profile\')]").click()
