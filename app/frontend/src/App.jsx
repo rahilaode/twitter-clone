47354a9fbd34376d5d759aaf3feb16169bb03b28
rahilaode <laoderahilatansa@gmail.com>
2024-05-29 16:59:18 +0700

    test fix bug

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -14,26 +14,6 @@ import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
-	const { data: authUser, isLoading } = useQuery({
-		// we use queryKey to give a unique name to our query and refer to it later
-		queryKey: ["authUser"],
-		queryFn: async () => {
-			try {
-				const res = await fetch("/api/auth/me");
-				const data = await res.json();
-				if (data.error) return null;
-				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong");
-				}
-				console.log("authUser is here:", data);
-				return data;
-			} catch (error) {
-				throw new Error(error);
-			}
-		},
-		retry: false,
-	});
-
 	if (isLoading) {
 		return (
 			<div className='h-screen flex justify-center items-center'>
